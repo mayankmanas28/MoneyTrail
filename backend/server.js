f41b45dce968827abc3f06a27ef75d0ba544d272
mayankmanas28 <145243409+mayankmanas28@users.noreply.github.com>
2025-10-29 01:16:12 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const app = express();
 
 const allowedOrigins = [
   "http://localhost:5173",
- "https://moneytrail-frontend.onrender.com",
+ "https://moneytrail-1.onrender.com",
 ];
 
 app.use(cors({
